9fc5959ab3f44eceea197a1601de0ff55e57c403
odjhey <odjhey@gmail.com>
2023-10-27 14:08:04 +0800

    feat: add menu and breads

--- web/src/layouts/MainLayout/MainLayout.tsx
+++ web/src/layouts/MainLayout/MainLayout.tsx
@@ -1,25 +1,9 @@
-import { Link, routes } from '@redwoodjs/router'
-
 type MainLayoutProps = {
   children?: React.ReactNode
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
-  return (
-    <div>
-      <div className="navbar">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link to={routes.jobs()}>Jobs</Link>
-          </li>
-          <li>
-            <Link to={routes.tasks()}>Tasks</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="p-2">{children}</div>
-    </div>
-  )
+  return <>{children}</>
 }
 
 export default MainLayout
